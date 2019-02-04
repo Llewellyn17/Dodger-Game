ba6864abfafb498f88f9ef18fba152dcd7729ef2
Student <student@Admins-iMac.local>
2019-02-04 10:48:19 -0600

    90% fininshed prodeuct

--- ap.js
+++ ap.js
@@ -3,12 +3,14 @@ w3school heavily added
 **********************/
 var myGamePiece;//intialize game piece
 var FallingObject = [];
+var TheScore;
 function startGame() {
     myGamePiece = new player(30, 30, "blue", 230, 440);
-    FallingObject.push(new player(10, 10, "white", 10, 10))
-    FallingObject.push(new player(10, 10, "white", 150, 300))
-    FallingObject.push(new player(10, 10, "white", 100, 70))
-    FallingObject.push(new player(10, 10, "white", 300, 220))
+    TheScore = new player ("30px", "consolas", "White",300, 40, "text");
+    // FallingObject.push(new player(10, 10, "white", 10, 10))
+    // FallingObject.push(new player(10, 10, "white", 150, 300))
+    // FallingObject.push(new player(10, 10, "white", 100, 70))
+    // FallingObject.push(new player(10, 10, "white", 300, 220))
     myGameArea.start();
     /**************************************************
      The in order width, height, color, x-axis,y-axis
@@ -37,7 +39,8 @@ function everyinterval(n) {
     if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
     return false;
   }
-function player(width, height, color, x, y) {
+function player(width, height, color, x, y, type) {
+    this.type = type;
     this.width = width;
     this.height = height;
     this.speedX = 0;
@@ -46,9 +49,15 @@ function player(width, height, color, x, y) {
     this.y = y;
     this.update = function () {
         ctx = myGameArea.context;
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+          } else {
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+}
     this.newPos = function () {
         this.x += this.speedX;
         this.y += this.speedY;
@@ -122,6 +131,19 @@ function updateGameArea() {
     }
     myGameArea.clear();
     myGameArea.frameNo += 1;
+    if (myGameArea.frameNo == 1 || everyinterval(150)){
+        y = myGameArea.canvas.height;
+        minWidth = 20;
+        maxWidth = 480;
+        //mutiple randoms added to give well more randomness
+        width = Math.floor(Math.random()*(maxWidth - minWidth + 1)+ minWidth);
+FallingObject.push(new player (10,10, "red", width, 0));
+width2 = Math.floor(Math.random()*(maxWidth - minWidth + 1)+ minWidth);
+FallingObject.push(new player (10,10, "blue", width2 , 0));
+width3 = Math.floor(Math.random()*(maxWidth - minWidth + 1)+ minWidth);
+FallingObject.push(new player (10,10, "yellow", width3 , 0));
+
+    }
 
     //showing all dots
     for (i = 0; i < FallingObject.length; i++) {
@@ -129,25 +151,75 @@ function updateGameArea() {
         //moving white dots
         FallingObject[i].y = FallingObject[i].y + .5;
     }
+    TheScore.text = "SCORE: " + myGameArea.frameNo;
+    TheScore.update();
     myGamePiece.newPos();
     myGamePiece.update();
 }
 
-function moveup() {
-    myGamePiece.speedY = -1;
+ document.addEventListener("keydown",function(e){
+    switch (event.keyCode) {
+        case 37: // Left
+          moveLeft();
+        break;
+    
+        case 38: // Up
+          moveUp();
+        break;
+    
+        case 39: // Right
+          moveRight();
+        break;
+    
+        case 40: // Down
+          moveDown();
+        break;
+      }
+    }, false);
+/*****************************************************
+ * Stops from having continuous movement on the board 
+  ****************************************************/
+    document.addEventListener("keyup",function(e){
+        switch (event.keyCode) {
+            case 37: 
+              clearMove();
+            break;
+        
+            case 38:
+              clearMove();
+            break;
+        
+            case 39:
+              clearMove();
+            break;
+        
+            case 40: 
+              clearMove();
+            break;
+          }
+        }, false);
+   
+function moveUp() {
+    myGamePiece.speedY -= 1;
 }
 
-function movedown() {
-    myGamePiece.speedY = 1;
+function moveDown() {
+    myGamePiece.speedY += 1;
 }
 
-function moveleft() {
-    myGamePiece.speedX = -1;
+function moveLeft() {
+    myGamePiece.speedX -= 1;
 }
 
-function moveright() {
-    myGamePiece.speedX = 1;
+function moveRight() {
+    myGamePiece.speedX += 1;
 }
+
+
+
+
+
+
 //stops object from moving continously in the direction last hit. has to be linked back to html
 function clearMove() {
     myGamePiece.speedX = 0;
